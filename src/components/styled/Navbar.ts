19facc2102543b7d0d0b8d28dffe66b98ba46332
jpcanto <jplopessk8@gmail.com>
2020-11-12 21:25:08 -0300

    layout: creating basic navbar styled element

---
+++ src/components/styled/Navbar.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Navbar = styled.nav`
+    font-family: sans-serif;
+    font-weight: 600;
+
+    align-items: center;
+    background: transparent;
+    display: flex;
+    height: 4vw;
+    justify-content: space-evenly;
+
+    a {
+        text-decoration: initial;
+    }
+`;
