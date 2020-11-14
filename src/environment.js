33360c1dde33f8a9d977b64f0aa49925417300f8
jpcanto <jplopessk8@gmail.com>
2020-11-14 01:41:57 -0300

    feat: adding environment for global variables

---
+++ src/environment.js
@@ -0,0 +1,9 @@
+const baseUrl = 'https://swapi.dev/api'
+
+const URL_API = {
+    getFilms: `${baseUrl}/films`,
+    getCharacters: `${baseUrl}/people`,
+    getPlanets: `${baseUrl}/planets`
+}
+
+export default URL_API;
