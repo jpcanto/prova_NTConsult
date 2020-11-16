e7b12fca92961ef821740d6864f9508ea68cf01c
jpcanto <jplopessk8@gmail.com>
2020-11-16 00:39:00 -0300

    feat: adding some route tests

--- src/views/Main/Main.test.tsx
+++ src/views/Main/Main.test.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Main from './Main';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
 
-test('renders learn react link', () => {
+import '@testing-library/jest-dom/extend-expect';
+
+import Main from './Main';
+import Routes from '../../routes';
+
+test('Should render mains title', () => {
   render(<Main />);
   const linkElement = screen.getByText(/Who's watching Star-Wars-Flix\?/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('Should change to films view', () => {
+  const history = createMemoryHistory();
+
+  render(<Router history={history}>
+    <Routes />
+  </Router>)
+
+  let navigation = screen.getByText(/films/i);
+
+  expect(navigation).toBeInTheDocument();
+  userEvent.click(navigation, { button: 0 });
+  expect(screen.getByRole('films')).toBeInTheDocument();
+});
+
+test('Should change to characters view', () => {
+  const history = createMemoryHistory();
+
+  render(<Router history={history}>
+      <Routes />
+    </Router>)
+
+  let navigation = screen.getByText(/characters/i);
+
+  expect(navigation).toBeInTheDocument();
+  userEvent.click(navigation, { button: 0 });
+  expect(screen.getByRole('characters')).toBeInTheDocument();
+
 });
